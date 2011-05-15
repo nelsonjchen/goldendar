475658c5e01dc2421abfcaf3f944a8e5dd37b997
Nelson Chen <crazysim@gmail.com>
2011-05-15 05:02:35 -0700

    parses schedule

--- src/main/java/com/mindflakes/goldendar/GoldScraper.java
+++ src/main/java/com/mindflakes/goldendar/GoldScraper.java
@@ -4,6 +4,8 @@ import org.htmlcleaner.CleanerProperties;
 import org.htmlcleaner.HtmlCleaner;
 import org.htmlcleaner.TagNode;
 
+import java.util.List;
+
 /**
  * Created by IntelliJ IDEA.
  * User: nelson
@@ -12,18 +14,34 @@ import org.htmlcleaner.TagNode;
  * To change this template use File | Settings | File Templates.
  */
 public class GoldScraper {
+    Schedule schedule;
+
     public Schedule parseContent(String content) throws Exception{
         HtmlCleaner cleaner = new HtmlCleaner();
         CleanerProperties properties = cleaner.getProperties();
         properties.setOmitComments(true);
-        TagNode response = cleaner.clean(content);
-        TagNode[] schedule = response.getElementsByAttValue("id","ctl00_pageContent_CourseList",true,true);
-        TagNode[] tbody = schedule[0].getElementsByName("tbody",false);
-        for (int i = 1; i < tbody.length; i++){
+        properties.setTransSpecialEntitiesToNCR(true);
+        properties.setRecognizeUnicodeChars(true);
+        properties.setTransResCharsToNCR(true);
 
+        schedule = new Schedule();
+
+        TagNode response = cleaner.clean(content);
+        TagNode[] schedule_table = response.getElementsByAttValue("id","ctl00_pageContent_CourseList",true,true);
+        List<TagNode> tr_course = schedule_table[0].getElementsByName("tbody",false)[0].getChildren();
+        for (int i = 1; i < tr_course.size(); i++){
+            Course course = new Course();
+            TagNode tr = tr_course.get(i);
+            TagNode[] title_elements = tr.getElementsByAttValue("style","padding-left:3px;" +
+                    " font-weight:bold;" +
+                    " font-size:10px;" +
+                    " float: left;", true, false);
+            String title = title_elements[0].getText().toString();
+            course.setName(title);
+            schedule.getCourses().add(course);
         }
 
 
-        return null;
+        return schedule;
     }
 }
--- src/main/java/com/mindflakes/goldendar/Schedule.java
+++ src/main/java/com/mindflakes/goldendar/Schedule.java
@@ -12,11 +12,11 @@ import java.util.ArrayList;
 public class Schedule {
     private ArrayList<Course> courses;
 
-    public ArrayList<Course> getCourses() {
-        return courses;
+    public Schedule() {
+        courses = new ArrayList<Course>();
     }
 
-    public void setCourses(ArrayList<Course> courses) {
-        this.courses = courses;
+    public ArrayList<Course> getCourses() {
+        return courses;
     }
 }
