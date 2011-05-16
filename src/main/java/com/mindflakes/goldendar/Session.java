c612603a542ecb0e36ee48632d6a95b84cf3657d
Nelson Chen <crazysim@gmail.com>
2011-05-16 13:01:06 -0700

    session class to hold times

---
+++ src/main/java/com/mindflakes/goldendar/Session.java
@@ -0,0 +1,11 @@
+package com.mindflakes.goldendar;
+
+/**
+ * Created by IntelliJ IDEA.
+ * User: nelson
+ * Date: 5/16/11
+ * Time: 1:00 PM
+ * To change this template use File | Settings | File Templates.
+ */
+public class Session {
+}
