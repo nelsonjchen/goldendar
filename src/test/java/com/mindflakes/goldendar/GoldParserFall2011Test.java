9845418ff66c83b3d66770e8b3869dd810631ae9
Nelson Chen <crazysim@gmail.com>
2011-05-17 00:32:30 -0700

    copy to 2011

---
+++ src/test/java/com/mindflakes/goldendar/GoldParserFall2011Test.java
@@ -0,0 +1,42 @@
+package com.mindflakes.goldendar;
+
+import org.junit.Test;
+
+import static org.hamcrest.CoreMatchers.is;
+import static org.hamcrest.CoreMatchers.not;
+import static org.hamcrest.number.OrderingComparison.greaterThan;
+import static org.junit.Assert.assertThat;
+
+/**
+ * Created by IntelliJ IDEA.
+ * User: nelson
+ * Date: 5/14/11
+ * Time: 6:12 PM
+ * To change this template use File | Settings | File Templates.
+ */
+public class GoldParserFall2011Test extends Base {
+
+    @Test
+    public void testTrue(){
+        assertThat(true,is(true));
+    }
+
+    @Test
+    public void testFalse(){
+        assertThat(false,is(not(true)));
+    }
+
+    @Test
+    public void testFileIsRead() throws Exception{
+        StringBuffer content = loadContent("fall2011.html");
+        assertThat(content.toString().length(),is(greaterThan(10)));
+    }
+
+    @Test
+    public void testSchedule() throws Exception {
+        StringBuffer content = loadContent("fall2011.html");
+        GoldScraper scraper = new GoldScraper();
+        scraper.parseContent(content.toString());
+    }
+
+}
