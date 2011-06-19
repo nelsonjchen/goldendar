05e50f63711ff9dcff85591d4913a03901b5bb3e
Nelson Chen <crazysim@gmail.com>
2011-06-18 20:06:49 -0700

    simpilifed template

--- src/main/java/com/mindflakes/goldendar/Schedule.java
+++ src/main/java/com/mindflakes/goldendar/Schedule.java
@@ -2,13 +2,6 @@ package com.mindflakes.goldendar;
 
 import java.util.ArrayList;
 
-/**
- * Created by IntelliJ IDEA.
- * User: nelson
- * Date: 5/15/11
- * Time: 2:29 AM
- * To change this template use File | Settings | File Templates.
- */
 public class Schedule {
     private ArrayList<Course> courses;
     private String name;
