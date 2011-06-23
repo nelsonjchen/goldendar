f28548901f855e3cb77beb5ea61e34cbb6b78c09
Nelson Chen <crazysim@gmail.com>
2011-06-23 00:24:40 -0700

    initial commit for academic calendar test

---
+++ src/test/java/com/mindflakes/goldendar/AcademicCalendar2011Test.java
@@ -0,0 +1,11 @@
+package com.mindflakes.goldendar;
+
+/**
+ * Created by IntelliJ IDEA.
+ * User: nelson
+ * Date: 6/23/11
+ * Time: 12:24 AM
+ * To change this template use File | Settings | File Templates.
+ */
+public class AcademicCalendar2011Test {
+}
