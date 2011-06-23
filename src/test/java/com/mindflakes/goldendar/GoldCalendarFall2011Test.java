80e635c33ee54991b57687765ea2cb80df9f9ce5
Nelson Chen <crazysim@gmail.com>
2011-06-22 23:06:31 -0700

    we just want a string

--- src/test/java/com/mindflakes/goldendar/GoldCalendarFall2011Test.java
+++ src/test/java/com/mindflakes/goldendar/GoldCalendarFall2011Test.java
@@ -2,12 +2,16 @@ package com.mindflakes.goldendar;
 
 import org.junit.Test;
 
+import java.io.IOException;
+
 import static org.hamcrest.CoreMatchers.is;
 import static org.hamcrest.CoreMatchers.not;
 import static org.hamcrest.number.OrderingComparison.greaterThan;
 import static org.junit.Assert.assertThat;
 
-public class GoldCalendarFall2011Test extends GoldParserFall2011Test {
+public class GoldCalendarFall2011Test extends Base {
 
-
+    private String getScheduleString() throws IOException{
+        return loadContent("fall2011.html").toString();
+    }
 }
