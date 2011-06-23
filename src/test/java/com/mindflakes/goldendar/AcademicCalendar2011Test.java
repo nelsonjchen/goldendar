239693020c28d6627c642cf41a314eaad9036b37
Nelson Chen <crazysim@gmail.com>
2011-06-23 07:58:45 -0700

    more work done on test

--- src/test/java/com/mindflakes/goldendar/AcademicCalendar2011Test.java
+++ src/test/java/com/mindflakes/goldendar/AcademicCalendar2011Test.java
@@ -1,11 +1,4 @@
 package com.mindflakes.goldendar;
 
-/**
- * Created by IntelliJ IDEA.
- * User: nelson
- * Date: 6/23/11
- * Time: 12:24 AM
- * To change this template use File | Settings | File Templates.
- */
-public class AcademicCalendar2011Test {
+public class AcademicCalendar2011Test extends Base {
 }
