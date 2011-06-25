a9bd82ad807a6850caceed7c58a05e8bf095da1e
Nelson Chen <crazysim@gmail.com>
2011-06-25 12:31:37 -0700

    quarter and holidays

--- src/main/java/com/mindflakes/goldendar/calendar/AcademicCalendar.java
+++ src/main/java/com/mindflakes/goldendar/calendar/AcademicCalendar.java
@@ -1,4 +1,14 @@
 package com.mindflakes.goldendar.calendar;
 
 public class AcademicCalendar {
+    private String quarter;
+
+    public AcademicCalendar(String quarter){
+        this.quarter = quarter;
+    }
+
+    public String getQuarter() {
+        return quarter;
+    }
+
 }
---
+++ src/main/java/com/mindflakes/goldendar/calendar/Holiday.java
@@ -0,0 +1,11 @@
+package com.mindflakes.goldendar.calendar;
+
+/**
+ * Created by IntelliJ IDEA.
+ * User: nelson
+ * Date: 6/25/11
+ * Time: 1:26 AM
+ * To change this template use File | Settings | File Templates.
+ */
+public class Holiday {
+}
